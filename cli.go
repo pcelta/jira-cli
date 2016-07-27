760fb819764e9ab0f2a46d88553792e72b932d43
Pedro Ribeiro <pedro.neto@dafiti.com.br>
2016-07-26 23:10:00 -0300

    Showing informations about issue

--- cli.go
+++ cli.go
@@ -1,6 +1,5 @@
 package main
 
-import "fmt"
 import "os"
 //port "github.com/davecgh/go-spew/spew"	
 
@@ -8,11 +7,6 @@ func main() {
 	component := os.Args[1]
 
 	if component == "issue" {
-
-		endpoint := "issue/GFG-7586"
-		message := new(Message)
-		doRequest(endpoint, message)		
-
-		fmt.Println(message)
+		RunIssue()
 	}
 }
--- issue.go
+++ issue.go
@@ -3,14 +3,16 @@ package main
 import "os"
 import "fmt"
 
-type Message struct {
+type Issue struct {
 	Fields struct {
 		Assignee struct {
-			Self string `json:"self"`
-			Name string `json:"Name"`
+			Self string
+			Name string
+			DisplayName string
 		}
 		Reporter struct {
 			Name string
+			DisplayName string
 
 		}
 		Components []struct {
@@ -24,13 +26,40 @@ type Message struct {
 		Status struct {
 			Name string
 		}
+		Summary string
+		CreatedAt string `json:"created"`
 	}	
 }
 
-type Issue struct {
-
+func (i *Issue) show() {
+	fmt.Println("-------------------------------------------------")
+	fmt.Printf("Summary      : %s\n", i.Fields.Summary)
+	fmt.Printf("Reporter     : %s (user: %s)\n", i.Fields.Reporter.DisplayName, i.Fields.Reporter.Name)
+	fmt.Printf("Assinged to  : %s (user: %s)\n", i.Fields.Assignee.DisplayName, i.Fields.Assignee.Name)
+	var fixVersions string;
+	for _, version := range i.Fields.FixVersions {
+		fixVersions += version.Name + ","
+	}
+	fmt.Printf("Fix Versions : %s\n", fixVersions)
+	var components string;
+	for _, comp := range i.Fields.Components {
+		components += comp.Name + ","
+	}
+	fmt.Printf("Components   : %s\n", components)
+	fmt.Printf("Status       : %s\n", i.Fields.Status.Name)
+	fmt.Printf("Created At   : %s\n", i.Fields.CreatedAt)
+	fmt.Println("-------------------------------------------------")
 }
-func (i *Issue) Run() {
-	params := os.Args[2:]
-	fmt.Println(params)
+
+func RunIssue() {
+	action := os.Args[2]
+	if action == "get" {
+		id := os.Args[3]
+		//endpoint := "issue/GFG-7586"
+		endpoint := "issue/" + id
+		issue := new(Issue)
+		doRequest(endpoint, issue)
+
+		issue.show()
+	}
 }
